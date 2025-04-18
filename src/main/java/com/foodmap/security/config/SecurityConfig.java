c57d69a25291878810f03448c09f597fcad7e354
xiejunxiang <1923213756@qq.com>
2025-04-18 22:42:53 +0800

    修复redis异常，增加mybatis-plus

--- src/main/java/com/foodmap/security/config/SecurityConfig.java
+++ src/main/java/com/foodmap/security/config/SecurityConfig.java
@@ -2,21 +2,19 @@ package com.foodmap.security.config;
 
 import com.foodmap.security.jwt.JwtAuthenticationFilter;
 import com.foodmap.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -26,19 +24,15 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final JwtTokenProvider tokenProvider;
 
-    public SecurityConfig(UserDetailsService userDetailsService, JwtTokenProvider tokenProvider) {
-        this.userDetailsService = userDetailsService;
-        this.tokenProvider = tokenProvider;
-    }
-
     @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(tokenProvider);
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(tokenProvider, userDetailsService);
     }
 
     @Bean
